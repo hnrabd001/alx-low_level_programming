33df56d3b8ef14fa8a784e1fc8409210ea1df6ac
Abdrohman <abdx2001@gmail.com>
2022-04-07 13:16:08 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print "Programming is like building a multilingual puzzle, followed by a new line"
  *
  * Return: Always 0 (Success)
- */
-/**
- * main - Print "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: 0.
  */
 int main(void)
 {
