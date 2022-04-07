31d214affd430c55592a7bff02468856a5bc62df
Abdrohman <abdx2001@gmail.com>
2022-04-07 12:32:35 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/* 
+ *Use the function puts
+*You are not allowed to use printf
+*Your program should end with the value 0
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
