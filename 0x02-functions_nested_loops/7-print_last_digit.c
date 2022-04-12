197a58b6bc14d949297458b31a33670e0ee40eea
Abdrohman <abdx2001@gmail.com>
2022-04-12 13:39:59 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,8 @@
 /*
  * File: 7-print_last_digit.c
- * Abdrohman 
+ * Abdrohman
  */
-
 #include "main.h"
-
 /**
  * print_last_digit - Prints the last digit of a number.
  * @n: The number in question.
