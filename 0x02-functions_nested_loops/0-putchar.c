9b7727d23fef89eca114d7a19fb9fa45ce45c7b7
Abdrohman <abdx2001@gmail.com>
2022-04-12 01:56:11 -0700

    _puntchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-putchar.c
+ * Abdrohman
+ *
+ */
+
+#include "A.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('\n');
+	return (0);
+}
