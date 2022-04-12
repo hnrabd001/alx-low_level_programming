c8020a912276d7de83a1d31108fdc2f7cbfc6fc3
Abdrohman <abdx2001@gmail.com>
2022-04-12 13:30:48 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * File: 10-add.c
+ * Author Abdrohman Ebrahim Hanur
+ */
+
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: always 0.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
