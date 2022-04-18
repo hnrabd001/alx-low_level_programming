f6e44f55247cf3b433d4bb7c0f978c98930dfc9b
Abdrohman <abdx2001@gmail.com>
2022-04-18 01:48:14 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -16,11 +16,11 @@ void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *str);
+void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *s);
 int _atoi(char *s);
 
 #endif
