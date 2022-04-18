cc7a427a409c3b57b5a3bbb76a52068e6ed508c9
Abdrohman <abdx2001@gmail.com>
2022-04-18 02:17:52 -0700

    strig coppy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,36 +5,21 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating
- * null byte ('\0')
+ * _strcpy - copies the string pointed to by src, including t'\0')
  * @dest: the destination string
  * @src: the source string to be copied
  *
  * Return: the pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	int i;
+	int index = 0;
 
-	for (i = 0; i <= _strlen(src); i++)
-		dest[i] = src[i];
-	return (dest);
-}
-/**
- * _strlen - length of a string
- * @s: the string to check
- *
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int length;
-
-	length = 0;
-
-	for (length = 0; *s != '\0'; s++)
+	while (src[index])
 	{
-		length++;
+		dest[index] = src[index];
+		index++;
 	}
-	return (length);
+
+	return (dest);
 }
